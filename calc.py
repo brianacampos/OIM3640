efbf1948d2a7f4c5f8f4aff10a16d40f69f2c935
brianacampos <kcampos1@babson.edu>
2023-09-01 19:16:41 -0400

    in class

---
+++ calc.py
@@ -0,0 +1,19 @@
+#39+41+33
+print(39 + 41 + 33)
+print(2021 - 2000)
+print(15/3)
+
+
+#Question 1
+#First we need to convert minutes to seconds
+print((42*60)+42)
+
+#Question 2
+print(10/1.61)
+
+#Question 3
+#First divide total time by total distance
+2562/6.211
+412.482/60
+2562/3600
+6.211/0.7117
---
+++ hello.py
@@ -0,0 +1,2 @@
+print('Hello, World')
+#If you forgot to close the parenthesis or the quotation, it will not print as you would like for it to#
