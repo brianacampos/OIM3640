0945bf3cb37cc528c9dc307a90162f7b42c79fd8
brianacampos <kcampos1@babson.edu>
2023-10-02 15:17:28 -0400

    fixed indentation issue

--- session08/exercise3.py
+++ session08/exercise3.py
@@ -1,46 +1,50 @@
 import math
-epsilon= 0.00000000001
+epsilon = 0.00000000001
+
 
 def mysqrt(a):
     """
     takes a as a parameter, chooses a reasonable value of x, 
     and returns an estimate of the square root of a.
     """
-    x= a/2
+    x = a/2
     while True:
-      y = (x + a/x) / 2
-      if abs(y-x) < epsilon:
-        return y
-        break
-      x = y
+        y = (x + a/x) / 2
+        if abs(y-x) < epsilon:
+            return y
+            break
+        x = y
+
 
 def right_digit(n):
-  '''Takes a floating point number and returns its right
-  most digit in 'str' format'''
-  trunc= f'{n:.11f}'
-  dig= trunc[len(trunc)-1] 
-  return dig
+    '''Takes a floating point number and returns its right
+    most digit in 'str' format'''
+    trunc = f'{n:.11f}'
+    dig = trunc[len(trunc)-1]
+    return dig
+
 
 def test_square_root(n):
-  print('a', ' '*1, 'mysqrt(a)', ' '*3, 'math.sqrt(a)', ' diff')
-  print('-', ' '*1, '-'*9, ' '*3, '-'*12, ' ' + '-'*4)
-  
-  for i in range(9):
-    x= i+1
-    
-    print(f'{x:0.1f}', end=' ') 
-    
-    if mysqrt(x)- int(mysqrt(x)) < 0.001:
-      y=1      
-    elif right_digit(mysqrt(x)) == '0':
-      y=10
-    else:
-      y=11
-      
-    print(f'{mysqrt(x):<13.{y}f}', end=' ')
-    print(f'{mysqrt(x):<13.{y}f}', end=' ')
+    print('a', ' '*1, 'mysqrt(a)', ' '*3, 'math.sqrt(a)', ' diff')
+    print('-', ' '*1, '-'*9, ' '*3, '-'*12, ' ' + '-'*4)
 
-    diff= math.sqrt(x) - mysqrt(x)
-    print(f'{diff:.12g}')
+    for i in range(9):
+        x = i+1
+
+        print(f'{x:0.1f}', end=' ')
+
+        if mysqrt(x) - int(mysqrt(x)) < 0.001:
+            y = 1
+        elif right_digit(mysqrt(x)) == '0':
+            y = 10
+        else:
+            y = 11
+
+        print(f'{mysqrt(x):<13.{y}f}', end=' ')
+        print(f'{mysqrt(x):<13.{y}f}', end=' ')
+
+        diff = math.sqrt(x) - mysqrt(x)
+        print(f'{diff:.12g}')
+
 
 test_square_root(9)
